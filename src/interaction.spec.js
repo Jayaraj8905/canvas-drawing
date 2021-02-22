346d76e8da76d77b7ac31da25076994e3b1d89e3
Jayaraj <jayaraj8905@gmail.com>
2021-02-22 08:25:19 +0800

    Placeholder for pending specs

--- src/interaction.spec.js
+++ src/interaction.spec.js
@@ -1,5 +1,6 @@
-import { Canvas } from '../dist/canvas';
+import { Canvas } from './canvas';
 import { Interaction } from './interaction'; 
+// https://jestjs.io/docs/en/es6-class-mocks
 describe('Validation Messages', () => {
     const originalLog = console.log;
     afterEach(() => (console.log = originalLog));
@@ -7,6 +8,8 @@ describe('Validation Messages', () => {
         let consoleOutput = [];
         const mockedLog = output => consoleOutput.push(output);
         beforeEach(() => (console.log = mockedLog));
+
+        // TODO: EXPECT THE DRAW SHOULD NOT CALL
         test('Console log should be `No Canvas found. Create canvas first!` if we are inputting without creating the canvas', () => {
             consoleOutput = [];
             const interaction = new Interaction();
@@ -15,6 +18,7 @@ describe('Validation Messages', () => {
                 'No Canvas found. Create canvas first!'
             ]); 
         });
+        // TODO: EXPECT THE DRAW SHOULD NOT CALL
         test('Console log should be `Invalid Command!` if we are providing invalid commands', () => {
             consoleOutput = [];
             const interaction = new Interaction();
@@ -24,6 +28,7 @@ describe('Validation Messages', () => {
                 'Invalid Command!'
             ]); 
         });
+        // TODO: EXPECT THE DRAW SHOULD NOT CALL
         test('Console log should be `Canvas width and height should not be 0!` if we are providing dimensions with zero values', () => {
             consoleOutput = [];
             const interaction = new Interaction();
@@ -32,6 +37,25 @@ describe('Validation Messages', () => {
                 'Canvas width and height should not be 0!'
             ]); 
         });
+        // TODO: EXPECT THE DRAW SHOULD NOT CALL
+        test('Console log should be `Canvas width and height should not be 0!` if we are providing dimensions with zero values', () => {
+            consoleOutput = [];
+            const interaction = new Interaction();
+            interaction.validateAndHandle('C 0 4');
+            expect(consoleOutput).toEqual([
+                'Canvas width and height should not be 0!'
+            ]); 
+        });
+        // TODO: INVALID DIMENSIONS FOR LINE
+        // TODO: INBOUND FOR LINE
+
+        // TODO: INVALID DIMENSIONS FOR RECTANGLE
+        // TODO: INBOUND FOR RECTANGLE
+
+        // TODO: INBOUND FOR FILL
+
+        // TODO: PROMPT CONSOLE LOG CHECK
+
 
         
         
